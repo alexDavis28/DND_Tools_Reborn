75d2881428db4ff2cf9156ed8a14b98df7909a1c
Alex Davis <8m3xlt@gmail.com>
2020-11-14 15:16:04 +0000

    Added group roll

--- cogs/dice.py
+++ cogs/dice.py
@@ -2,6 +2,7 @@ import discord
 from discord.ext import commands
 import time
 import random
+import asyncio
 
 
 class Dice(commands.Cog):
@@ -24,13 +25,46 @@ class Dice(commands.Cog):
         rolls = []
         for i in range(count):
             rolls.append(random.randint(1, die))
-        total = sum(rolls)+modifier
+        total = sum(rolls) + modifier
         if sum(rolls) == 20 and count == 1 and die == 20:
-            await ctx.send(f"{ctx.author.mention} rolled a **{total}** with {count} {die}-sided dice:\n{rolls}\nand the modifier of:\n{modifier}\n*Critical success!*")
+            await ctx.send(
+                f"{ctx.author.mention} rolled a **{total}** with {count} {die}-sided dice:\n{rolls}\nand the modifier of:\n{modifier}\n*Critical success!*")
         elif sum(rolls) == 1 and count == 1 and die == 20:
-            await ctx.send(f"{ctx.author.mention} rolled a **{total}** with {count} {die}-sided dice:\n{rolls}\nand the modifier of:\n{modifier}\n*Critical failure!*")
+            await ctx.send(
+                f"{ctx.author.mention} rolled a **{total}** with {count} {die}-sided dice:\n{rolls}\nand the modifier of:\n{modifier}\n*Critical failure!*")
         else:
-            await ctx.send(f"{ctx.author.mention} rolled a **{total}** with {count} {die}-sided dice:\n{rolls}\nand the modifier of:\n{modifier}")
+            await ctx.send(
+                f"{ctx.author.mention} rolled a **{total}** with {count} {die}-sided dice:\n{rolls}\nand the modifier of:\n{modifier}")
+
+    @commands.command(aliases=["gr"])
+    async def group_roll(self, ctx, roll):
+
+        message = await ctx.send("React to this message within 20 seconds to join the roll")
+        await message.add_reaction("✅")
+
+        await asyncio.sleep(20)
+
+        roll = "".join([i for i in roll if i != " "])
+        parts = roll.split("d")
+        count = int(parts[0])
+        die = int(parts[1])
+
+        message = await ctx.channel.fetch_message(message.id)
+
+        reaction = message.reactions[0]
+
+        users = []
+
+        async for u in reaction.users():
+            users.append(u)
+
+        e = discord.Embed(title="Group roll", description=roll)
+        for user in users:
+            if user.bot:
+                continue
+            result = sum([random.randint(1, die) for i in range(count)])
+            e.add_field(name=user.name, value=result, inline=False)
+        await ctx.send(embed=e)
 
 
 def setup(client):
